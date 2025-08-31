055f941d4b11bec1b20dfa1c49b88114d27f8c8d
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-08-31 00:37:03 -0500

    Inicio

---
+++ Taller01/Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using Time;
+
+try
+{
+    var t1 = new Time();
+    var t2 = new Time(14);
+    var t3 = new Time(9, 34);
+    var t4 = new Time(19, 45, 56);
+    var t5 = new Time(23, 3, 45, 678);
+
+    var times = new List<Time> { t1, t2, t3, t4, t5 };
+
+    foreach (var time in times)
+    {
+        Console.WriteLine($"Time: {time}");
+        Console.WriteLine($"\tMilliseconds: {time.toMillisecond(),15:NO}");
+        Console.WriteLine($"\tSeconds     : {time.toSecond(),15:NO}");
+        Console.WriteLine($"\tMinutes     : {time.toMinutes(),15:NO}");
+        Console.WriteLine($"\t.add        : {time.add(t3),15:NO}");
+        Console.WriteLine($"\tIs other day: {time.IsOtherDay(t4)}");
+        Console.WriteLine();
+    }
+    var t6 = new Time(45, -7, 90, -87);
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
---
+++ Taller01/Time/Time.cs
@@ -0,0 +1,89 @@
+﻿namespace Time;
+
+public class Time
+{
+    private int _hour;
+    private int _millisecond;
+    private int _minute;
+    private int _second;
+
+
+
+    public Time(int hour, int millisecond, int minute, int second)
+    {
+
+        Hour = hour;
+        Millisecond = millisecond;
+        Minute = minute;
+        Second = second;
+    }
+
+    public int Hour
+    {
+        get => _hour;
+        set
+        {
+            _hour = ValidHour(value);
+        }
+    }
+
+    public int Millisecond
+    {
+        get => _millisecond;
+        set
+        {
+            _millisecond = ValidMillisecond(value);
+        }
+    }
+
+    public int Minute
+    {
+        get => _minute;
+        set
+        {
+            _minute = ValidMinute(value);
+        }
+    }
+    public int Second
+    {
+        get => _second;
+        set
+        {
+            _second = ValidSecond(value);
+        }
+    }
+
+    private int ValidHour(int hour)
+    {
+        if (hour < 0 || hour > 23)
+        {
+            throw new Exception("Hour must be between 0 and 23.");
+        }
+        return hour;
+    }
+    private int ValidMillisecond(int millisecond)
+    {
+        if (millisecond < 0 || millisecond > 999)
+        {
+            throw new Exception("Millisecond must be between 0 and 999.");
+        }
+        return millisecond;
+    }
+    private int ValidMinute(int minute)
+    {
+        if (minute < 0 || minute > 59)
+        {
+            throw new Exception("Minute must be between 0 and 59.");
+        }
+        return minute;
+    }
+    private int ValidSecond(int second)
+    {
+        if (second < 0 || second > 59)
+        {
+            throw new Exception("Second must be between 0 and 59.");
+        }
+        return second;
+
+    }
+}
